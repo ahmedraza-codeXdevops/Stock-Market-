f6aed6b26c16f2cb200672a956334b774551dc8f
Ahmed Raza <ahmedrazak2511@gmail.com>
2026-07-04 20:29:40 +0530

    Add files via upload

---
+++ Database .py
@@ -0,0 +1,52 @@
+stocks = {
+    "RELIANCE": {
+        "exchange": "NSE",
+        "price": 1520.50,
+        "change": 18.20
+    },
+    "TCS": {
+        "exchange": "NSE",
+        "price": 3850.40,
+        "change": -12.80
+    },
+    "INFY": {
+        "exchange": "NSE",
+        "price": 1675.25,
+        "change": 10.60
+    },
+    "HDFCBANK": {
+        "exchange": "NSE",
+        "price": 1988.70,
+        "change": 5.90
+    },
+    "ICICIBANK": {
+        "exchange": "NSE",
+        "price": 1425.30,
+        "change": -3.75
+    },
+    "SBIN": {
+        "exchange": "NSE",
+        "price": 912.40,
+        "change": 7.80
+    },
+    "TATAMOTORS": {
+        "exchange": "NSE",
+        "price": 702.80,
+        "change": -5.10
+    },
+    "ADANIENT": {
+        "exchange": "NSE",
+        "price": 2875.60,
+        "change": 20.40
+    },
+    "LT": {
+        "exchange": "NSE",
+        "price": 4012.50,
+        "change": 15.80
+    },
+    "ITC": {
+        "exchange": "NSE",
+        "price": 438.90,
+        "change": -1.25
+    }
+}
